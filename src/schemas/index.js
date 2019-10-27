3d3729c9e2f859adc8d8d1a2a01c678e3decb25f
Igor Motyka <igor_motyka@mail.com>
2019-10-27 22:04:09 +0100

    Added bot model

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,11 +2,14 @@ import { ConversationModel } from "./conversation";
 import { InvitationModel } from "./invitation";
 import { MessageModel } from "./message";
 import { UserModel, getUserModelPublicInfo } from "./user";
+import {BotModel, getBotModelPublicInfo} from "./bot";
 
 export {
     UserModel,
     getUserModelPublicInfo,
     MessageModel,
     InvitationModel,
-    ConversationModel
+    ConversationModel,
+    BotModel,
+    getBotModelPublicInfo
 }
